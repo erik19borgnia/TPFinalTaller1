0e8ad67a1b81aabc8339240d34194670f6ae033a
KireBook <KireBook@10.2.213.179>
2019-08-28 11:30:36 -0300

    Emmmm prueba?

---
+++ src/tpfinaltaller1/ClasePrueba.java
@@ -0,0 +1,23 @@
+package tpfinaltaller1;
+
+public class ClasePrueba extends Exception {
+    public ClasePrueba(String string, Throwable throwable, boolean b, boolean b1) {
+        super(string, throwable, b, b1);
+    }
+
+    public ClasePrueba(Throwable throwable) {
+        super(throwable);
+    }
+
+    public ClasePrueba(String string, Throwable throwable) {
+        super(string, throwable);
+    }
+
+    public ClasePrueba(String string) {
+        super(string);
+    }
+
+    public ClasePrueba() {
+        super();
+    }
+}
