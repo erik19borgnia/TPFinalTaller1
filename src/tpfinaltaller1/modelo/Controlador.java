bf3eda3185a9a1f5e29691bec2cdc1a5e5b0c3d3
KireBook <KireBook@10.2.210.95>
2019-09-04 09:29:05 -0300

    Version 0.1

---
+++ src/tpfinaltaller1/modelo/Controlador.java
@@ -0,0 +1,38 @@
+package tpfinaltaller1.modelo;
+
+public class Controlador {
+    public Controlador() {
+        super();
+    }
+    
+    private Almacen almacenActual;
+    
+    private void crear(String nombreAlmacen){
+        
+    }
+    
+    private void cargar(String nombreAlmacen){
+        
+    }
+    
+    private void guardar(){
+        
+    }
+    
+    private void importar(String nombreArchivo){
+        
+    }
+    
+    private void eliminar(int idAnalisis){
+        
+    
+    }
+    
+    private void consultar(String parametro){
+        
+    }
+    
+    public void analizarComando(){
+        
+    }
+}
